19ae571d1691bbf9fa2ebd57d16c7709277b9823
toshi7103 <toshi7103@gmail.com>
2022-04-29 15:45:12 +0900

    コンソールでイベントの確認

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let lists = document.getElementsByClassName("pull-down-list");
+console.log(lists);
+
+function getElement(){
+  element = document.querySelector('#shopB');
+  console.log('選ばれたのは' + element.textContent + 'でした');
+}
+
