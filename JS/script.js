ae8fdd5a1b02827c316b99dcd5b16e075db66111
toshi7103 <toshi7103@gmail.com>
2022-05-02 16:06:41 +0900

    インラインスタイル編
    JS基礎10

--- JS/script.js
+++ JS/script.js
@@ -11,14 +11,28 @@ window.addEventListener('load',function(){
 lists.addEventListener('mouseover',function(){
 
   console.log("乗る")
+  
 })
 
 lists.addEventListener('mouseout',function(){
 
   console.log("外れる")
+  
 })
 
 lists.addEventListener('click',function(){
 
   console.log("クリック")
+})
+
+pullDownButton.addEventListener('mouseover', function(){
+  pullDownButton.setAttribute("style", "background-color:blue;")
+})
+
+pullDownButton.addEventListener('mouseout',function(){
+  pullDownButton.removeAttribute("style", "background-color:red;")
+})
+
+pullDownButton.addEventListener('click', function(){
+  pullDownButton.setAttribute("style", "background-color:green;")
 })
